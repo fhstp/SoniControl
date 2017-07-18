89d48ca294ca9239e04ad78b73ee7ae421a05668
aringot <alexis.ringot@fhstp.ac.at>
2017-07-18 15:36:17 +0200

    Matthias cleaned up and commented. Moved specs setting out of the main loop.

--- octaveScripts/energyDetection_rms_test_v2.m
+++ octaveScripts/energyDetection_rms_test_v2.m
@@ -2,6 +2,8 @@ close all
 clear all
 clc
 
+%% Initialization
+
 %Matthias:
 %cd 'D:\Clouds\Dropbox (FH St. Pölten)\SoniControlShare\matlab_code\detection'
 %addpath('D:\Clouds\Dropbox (FH St. Pölten)\SoniControlShare\matlab_code');
@@ -14,15 +16,38 @@ if isOctave
 end
 
 %Florian: 
-folder = 'C:\Users\SoniControl#2\Desktop\detectionNoise';
+%folder = 'C:\Users\SoniControl#2\Desktop\detectionNoise';
 %Matthias: 
 %folder = 'D:\Clouds\Dropbox (FH St. Pölten)\SoniControlShare\Audio\NoiseAudio'
+%Alexis:
+folder = 'C:\Development\Audio_Data\nearby\fhstp\noisy'
 
 d = dir([folder filesep '*.wav']);
 nFiles = size(d,1);
 
-%%
-mainLoop(folder,d);
+fs=44100; %Set this here globally! We work only with this sampling rate
+
+%% Set Parameters for the detector
+specs.detector.sizeBuffer=200; %ms Windowsize (200 necessary for nearby)!
+specs.detector.rmsBufferSize = 20; %Number of collected RMS values in the buffer, unit = number of buffers
+specs.detector.medianBufferSize = 15; %Number of collected individual decisions (detections), unit = number of buffers
+specs.detector.cutoffFrequency = 18000;
+
+%% Set Parameters for the recognizer: Specification of different transmission technologies:
+%Google Nearby:
+specs.nearby.nBands=64;
+specs.nearby.bw=(20000-18500)./specs.nearby.nBands*2;
+specs.nearby.centerFreq = linspace(18500,20000,specs.nearby.nBands);
+
+%Prontoly
+specs.prontoly.frequencies = [18430 18520 18690 18780 18955 19040 19380 19466 19726]; %Hz
+specs.prontoly.bandwidth = 40; %unit: Hz
+specs.prontoly.duration = 250; %ms %or longer (up to 500ms)
+specs.prontoly.spacing = 4000; %ms %spacing between two words
+
+
+%% Run Processing
+mainLoop(folder,d,specs);
 
 
 
--- octaveScripts/mainLoop.m
+++ octaveScripts/mainLoop.m
@@ -1,4 +1,4 @@
-function mainLoop(folder,d)
+function mainLoop(folder,d, specs)
 
 
 for idxFile = 1%:nFiles
@@ -11,50 +11,43 @@ for idxFile = 1%:nFiles
   [y,fs] = audioread([folder filesep d(idxFile).name]);
   %[y,fs] = audioread('I:\Datasets\SoniControl\audio_resources\internal-clean-root-captures\cardcase\cardcase_nearbyClean-1.wav');
   
- 
+  
   disp([folder filesep d(idxFile).name]);
   y=mean(y,2); %tansform to mono
   n_smpl=numel(y); %Number of single values of y
 
-    sizeBuffer=200; %ms Windowsize (200 necessary for nearby)!
-    sizeBuffer_smpl = round(fs/1000*sizeBuffer); %Number of Windowsamples
+    %prepare buffer
+    sizeBuffer_smpl = round(fs/1000*specs.detector.sizeBuffer); %Number of Windowsamples
     buffer = zeros(1,sizeBuffer_smpl);
     frequencyResolution = fs/2 / floor(sizeBuffer_smpl / 2);
     frequencies = 0:frequencyResolution:fs/2;
+    
+    %prepare rmsBuffer
+    rmsBuffer = zeros(1,specs.detector.rmsBufferSize);
 
+    %prepare medianBuffer
+    medianBuffer = zeros(1,specs.detector.medianBufferSize);
 
-    rmsBufferSize = 20; %Number of collected RMS values in the buffer, unit = number of buffers
-    rmsBuffer = zeros(1,rmsBufferSize);
+    %prepare bufferHistory
+    bufferHistory = zeros(sizeBuffer_smpl,specs.detector.medianBufferSize);
 
-    medianBufferSize = 15;
-    medianBuffer = zeros(1,medianBufferSize);
+    %prepare highpass filter
+    [b,a] = butter(20,specs.detector.cutoffFrequency/(fs./2),'high'); %Highpassfilter
+    specs.detector.cutoffFrequencyIdx = freq2idx(specs.detector.cutoffFrequency,fs,sizeBuffer_smpl);
 
-    bufferHistory = zeros(sizeBuffer_smpl,medianBufferSize);
-
-    [b,a] = butter(20,18000/(fs./2),'high'); %Highpassfilter
-
-
-    %Specification of different transmission technologies:
-    %Google Nearby:
-    specs.nearby.nBands=64;
-    specs.nearby.bw=(20000-18500)./specs.nearby.nBands*2;
-    specs.nearby.centerFreq = linspace(18500,20000,specs.nearby.nBands);
+    %compute some additional parameters for nearby:
     specs.nearby.centerFreqIdx = freq2idx(specs.nearby.centerFreq,fs,sizeBuffer_smpl);
     specs.nearby.centerFreqIdxDiff = median(diff(specs.nearby.centerFreqIdx));
     %specs.nearby.centerFreqIdx = sort([specs.nearby.centerFreqIdx-2 specs.nearby.centerFreqIdx-1 specs.nearby.centerFreqIdx+1 specs.nearby.centerFreqIdx+2]);
-    specs.nearby.offFreq = specs.nearby.centerFreq-(specs.nearby.bw/4);
+    specs.nearby.offFreq = specs.nearby.centerFreq-(specs.nearby.bw/4); %intermediate frequencies (between two nearby frequencies)
     specs.nearby.offFreqIdx = freq2idx(specs.nearby.offFreq,fs,sizeBuffer_smpl);
     %specs.nearby.offFreqIdx = sort([specs.nearby.offFreqIdx-2 specs.nearby.offFreqIdx-1 specs.nearby.offFreqIdx+1 specs.nearby.offFreqIdx+2]);
-    
-    %Prontoly
-    specs.prontoly.frequencies = [18430 18520 18690 18780 18955 19040 19380 19466 19726]; %Hz
-    specs.prontply.bandwidth = 40; %unit: Hz
-    specs.prontoly.duration = 250; %ms %or longer (up to 500ms)
-    specs.prontoly.spacing = 4000; %ms %spacing between two words
-    
+
   
-  %detNumber = 30; %Number of Detections for triggering the detector
+
   windowStartIdx = 1:sizeBuffer_smpl:n_smpl-sizeBuffer_smpl;
+  
+  %two helpers who store the detection results
   globalDetectionBuffer = zeros(1, numel(windowStartIdx))-1; %Creation of matrix with 0 only
   globalDetectionNearby = zeros(1, numel(windowStartIdx))-1; %Creation of matrix with 0 only
   
@@ -64,9 +57,15 @@ for idxFile = 1%:nFiles
   for idx=1:sizeBuffer_smpl:n_smpl-sizeBuffer_smpl
     
     buffer = y(idx:idx+sizeBuffer_smpl-1); %Current frame/current window is the point of y with the current index to the current index plus the windowsize minus 1 = always windowsize
-    bufferHiPass = filter(b,a,buffer); %Current frame filtered through the highpassfilter
-    rms = sqrt(mean((bufferHiPass.^2))); %RMS of the current frame
-    
+    %version A: with time-domain highpass filter
+    %bufferHiPass = filter(b,a,buffer); %Current frame filtered through the highpassfilter
+    %rms = sqrt(mean((bufferHiPass.^2))); %RMS of the current frame
+    %version B: with fourier transform (FFT)
+    bufferHiPass = abs(fft(buffer));
+    bufferHiPass = bufferHiPass(1:round(numel(bufferHiPass)/2));
+    bufferHiPass(1:specs.detector.cutoffFrequencyIdx) = 0;
+    rms = sqrt(mean(bufferHiPass.^2)); %RMS of the current frame
+    %TODO: consider only those values which are above the average amplitude in bufferHiPass: mean(bufferHiPass(specs.detector.cutoffFrequencyIdx+1:end))
     if counter > rmsBufferSize %start detection when rmsBufferSize is full
       bufferHistory(:,mod(j,medianBufferSize)+1) = buffer;
       
