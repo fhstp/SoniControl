f33f08e9f19155bb74ce0e35f0ab35d0d173551e
Florian <florian.taurer@gmx.at>
2018-02-11 23:42:36 +0100

    reset settings via the settings activity

--- SoniControl/app/src/main/java/at/ac/fhstp/sonicontrol/SettingsFragment.java
+++ SoniControl/app/src/main/java/at/ac/fhstp/sonicontrol/SettingsFragment.java
@@ -2,6 +2,7 @@ package at.ac.fhstp.sonicontrol;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.preference.PreferenceFragment;
 import android.preference.*;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ public class SettingsFragment extends PreferenceFragment {
     JSONManager jsonMan;
     MainActivity nextMain;
     AlertDialog alertDelete = null;
+    AlertDialog alertReset = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,6 +49,45 @@ public class SettingsFragment extends PreferenceFragment {
                         })
                         .setIcon(android.R.drawable.ic_dialog_alert);
                 alertDelete = deleteJsonDialog.show();
+                return false;
+            }
+        });
+
+        final Preference prefReset = findPreference(ConfigConstants.PREFERENCE_RESET_PREFERENCES);
+        prefReset.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                final AlertDialog.Builder resetSettingsDialog = new AlertDialog.Builder(getActivity());
+                resetSettingsDialog.setTitle("Reset Settings")
+                        .setMessage("Do you want to reset all settings?")
+                        .setCancelable(false)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(nextMain.getApplicationContext());
+                                SharedPreferences.Editor editor = preferences.edit();
+                                editor.putBoolean(ConfigConstants.SETTING_CONTINOUS_SPOOFING, ConfigConstants.SETTING_CONTINOUS_SPOOFING_DEFAULT);
+                                editor.putBoolean(ConfigConstants.SETTING_GPS, ConfigConstants.SETTING_GPS_DEFAULT);
+                                editor.putBoolean(ConfigConstants.SETTING_NETWORK_USE, ConfigConstants.SETTING_NETWORK_USE_DEFAULT);
+                                editor.putBoolean(ConfigConstants.SETTING_MICROPHONE_FOR_BLOCKING, ConfigConstants.SETTING_MICROPHONE_FOR_BLOCKING_DEFAULT);
+                                editor.putBoolean(ConfigConstants.SETTING_SAVE_DATA_TO_JSON_FILE, ConfigConstants.SETTING_SAVE_DATA_TO_JSON_FILE_DEFAULT);
+                                editor.putBoolean(ConfigConstants.SETTING_PREVENTIVE_SPOOFING, ConfigConstants.SETTING_PREVENTIVE_SPOOFING_DEFAULT);
+                                editor.putString(ConfigConstants.SETTING_LOCATION_RADIUS,ConfigConstants.SETTING_LOCATION_RADIUS_DEFAULT);
+                                editor.putString(ConfigConstants.SETTING_PULSE_DURATION,ConfigConstants.SETTING_PULSE_DURATION_DEFAULT);
+                                editor.putString(ConfigConstants.SETTING_PAUSE_DURATION,ConfigConstants.SETTING_PAUSE_DURATION_DEFAULT);
+                                editor.putString(ConfigConstants.SETTING_BANDWIDTH,ConfigConstants.SETTING_BANDWIDTH_DEFAULT);
+                                editor.putString(ConfigConstants.SETTING_BLOCKING_DURATION,ConfigConstants.SETTING_BLOCKING_DURATION_DEFAULT);
+                                editor.putBoolean(ConfigConstants.SETTINGS_ALERT_LOCATION_IS_OFF_DONT_ASK_AGAIN,ConfigConstants.SETTINGS_ALERT_LOCATION_IS_OFF_DONT_ASK_AGAIN_DEFAULT);
+                                editor.apply();
+                                editor.commit();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                alertReset.cancel();
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert);
+                alertReset = resetSettingsDialog.show();
                 return false;
             }
         });
