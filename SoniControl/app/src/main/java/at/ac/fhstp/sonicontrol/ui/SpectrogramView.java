4a53160a0c8a920991737dfed59fdb65c445cca0
aringot <alexis.ringot@fhstp.ac.at>
2019-07-10 12:40:04 +0200

    Adapative strokeWidth, so that the spectrogram always fit the screen.

--- SoniControl/app/src/main/java/at/ac/fhstp/sonicontrol/ui/SpectrogramView.java
+++ SoniControl/app/src/main/java/at/ac/fhstp/sonicontrol/ui/SpectrogramView.java
@@ -193,12 +193,14 @@ public class SpectrogramView extends View {
         int wFrequency = 63;
         int rWidth = width-wColor-wFrequency;
         int strokeWidth;
-        if (onlyShowUpperFrequencies) {
-            strokeWidth = 6; // 4 for an overlapFactor of 8.
+        strokeWidth = rWidth / historyBufferLength;
+        Log.d("onDraw", "strokeWidth " + strokeWidth);
+        /*if (onlyShowUpperFrequencies) {
+            strokeWidth = 4; // 4 for an overlapFactor of 8.
         }
         else {
             strokeWidth = 1; // With no overlap : 1 for the whole range of frequencies, 10 or 20 for the upper frequencies
-        }
+        }*/
         paint.setStrokeWidth(strokeWidth);
 
         // Get scale preferences
