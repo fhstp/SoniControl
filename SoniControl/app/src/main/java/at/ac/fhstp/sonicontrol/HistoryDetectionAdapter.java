e025857c031a4f165299d91a910d8e8ed37f218f
Florian Taurer <florian.taurer@gmx.at>
2020-01-30 16:15:24 +0100

    shorten latitude and longitude of history items

--- SoniControl/app/src/main/java/at/ac/fhstp/sonicontrol/HistoryDetectionAdapter.java
+++ SoniControl/app/src/main/java/at/ac/fhstp/sonicontrol/HistoryDetectionAdapter.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2018, 2019. Alexis Ringot, Florian Taurer, Matthias Zeppelzauer.
+ *
+ * This file is part of SoniControl app.
+ *
+ *     SoniControl app is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     SoniControl app is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with SoniControl app.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package at.ac.fhstp.sonicontrol;
 
 import android.content.Context;
@@ -34,8 +53,11 @@ public class HistoryDetectionAdapter extends ArrayAdapter<String[]> {
         TextView txtLastDet = (TextView) customView.findViewById(R.id.lastdetection);
         TextView txtAddress = (TextView) customView.findViewById(R.id.txtAddress);
 
-        txtLon.setText("Lon " + singleArrayItem[0]);
-        txtLat.setText("Lat " + singleArrayItem[1]);
+        txtLon.setText("Lon " + singleArrayItem[0].substring(0, 7));
+        txtLat.setText("Lat " + singleArrayItem[1].substring(0, 7));
+
+        //txtLon.setText("Lon " + singleArrayItem[0]);
+        //txtLat.setText("Lat " + singleArrayItem[1]);
         txtTech.setText(singleArrayItem[2]);
         String formattedDate = singleArrayItem[3];
         formattedDate = formattedDate.replace("T"," ");
